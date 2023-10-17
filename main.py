def1bf5e6c30eac84e7687b0f646e6098cf3f267
Thiago Melo <es.thg.melo@gmail.com>
2023-10-17 12:50:14 -0300

    feat: datas e timedelta

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from datas_br import DatasBr
-from datetime import datetime
+from datetime import datetime, timedelta
 
-'''
-hoje = datetime.today()
-hoje_formatado = hoje.strftime("%d/%m/%Y %H:%M")
-print(hoje)
-print(hoje_formatado)
-print(type(hoje_formatado))
-'''
-
-cadastro = DatasBr()
-print(cadastro)
+hoje = DatasBr()
+print(hoje.tempo_cadastro())
