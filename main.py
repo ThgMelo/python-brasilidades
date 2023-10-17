cc3f8d09b700cc7907fa69afd1914f50eb0af597
Thiago Melo <es.thg.melo@gmail.com>
2023-10-17 12:41:34 -0300

    feat: formatando datas

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from datas_br import DatasBr
+from datetime import datetime
+
+'''
+hoje = datetime.today()
+hoje_formatado = hoje.strftime("%d/%m/%Y %H:%M")
+print(hoje)
+print(hoje_formatado)
+print(type(hoje_formatado))
+'''
 
 cadastro = DatasBr()
-print(cadastro.mes_cadastro())
-print(cadastro.dia_semana())
+print(cadastro)
