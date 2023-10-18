df8b9eb85aaffec5e898847831c6638484e710f1
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-17 23:29:36 -0300

    feat: Respostas API Via CEP

--- acesso_cep.py
+++ acesso_cep.py
@@ -1,3 +1,5 @@
+import requests
+
 class BuscaEndereco:
     def __init__(self, cep):
         cep = str(cep)
@@ -5,9 +7,25 @@ class BuscaEndereco:
             self.cep = cep
         else:
             raise ValueError("CEP inválido!")
+    
+    def __str__(self):
+        return self.format_cep()
         
     def cep_eh_valido(self, cep):
         if len(cep) == 8:
             return True
         else:
             return False
+        
+    def format_cep(self):
+        return "{}-{}".format(self.cep[:5], self.cep[5:])
+    
+    def acessa_via_cep(self):
+        url = "http://viacep.com.br/ws/{}/json/".format(self.cep)
+        r = requests.get(url)
+        dados = r.json()
+        return (
+            dados['bairro'],
+            dados['localidade'],
+            dados['uf']
+        )
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import requests
 from acesso_cep import BuscaEndereco
 
-cep = 25870140
+cep = "01001000"
 objeto_cep = BuscaEndereco(cep)
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+
+print(bairro, cidade, uf)
